1b8f207268e53ef848300a143ba44339d0874227
tukusejssirs <bolyosovci@gmail.com>
2018-02-24 19:07:25 +0100

    Add bash_functions dir; add fram.sh

---
+++ bash_functions/.bash_functions/fram.sh
@@ -0,0 +1,10 @@
+# This script clears RAM
+# You can use any option program `free` uses in order to change its output.
+
+# author: Tukusej's Sirs (found on the Internet on multiple places)
+# created: 24 Feb 2018
+# dependencies; free sync sudo su echo
+# version: 1.0
+
+#!/bin/bash
+free $1; sync && sudo su -c "echo 2 > /proc/sys/vm/drop_caches" && echo && free $1
