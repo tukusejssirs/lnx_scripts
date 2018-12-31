d42c3a9a21be07d4cade91929f5dd13ff4ac2ef7
tukusejssirs <bolyosovci@gmail.com>
2018-12-31 19:13:13 +0100

    New Update

--- bash_functions/gup.sh
+++ bash_functions/gup.sh
@@ -4,11 +4,11 @@ function gup(){
 	test=`gs | tail -1`
 	okay="nothing to commit, working tree clean"
 
-	if [[ $test == $okay ]]; then
-		return $okay
-	else
-		git add --all
-		git commit -am $msg
-		git push
-	fi
+	# if [[ $test == $okay ]]; then
+	# 	return $okay
+	# else
+	# 	git add --all
+	# 	git commit -am $msg
+	# 	git push
+	# fi
 }
