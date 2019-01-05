1ba3143bc80b6a4cd7f95b52dea24042349e6913
tukusejssirs <bolyosovci@gmail.com>
2019-01-05 20:50:52 +0100

    New from Linux

---
+++ bash_init/bash_init.sh
@@ -0,0 +1,9 @@
+# Bash files initialisation script
+# version: 1.0
+
+#!/bin/bash
+
+ln -s $HOME/.bash_aliases $HOME/git/lnx_scripts/bash_aliases/.bash_aliases
+ln -s $HOME/.bash_profile $HOME/.bashrc
+ln -s $HOME/.bash_progs $HOME/git/lnx_scripts/bash_progs/.bash_progs
+ln -s $HOME/.bashrc $HOME/git/lnx_scripts/bashrc/.bashrc
