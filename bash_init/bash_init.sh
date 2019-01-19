c210f10636f212a604c5857770caede060351344
tukusejssirs <bolyosovci@gmail.com>
2019-01-19 23:49:20 +0100

    Add user-dirs; Update bash_init.sh

--- bash_init/bash_init.sh
+++ bash_init/bash_init.sh
@@ -1,15 +1,21 @@
 # Bash files initialisation script
-# version: 1.0
+# version: 1.1
 
 #!/bin/bash
-if [[ -e $HOME/.bash_aliases ]]; then mv $HOME/.bash_aliases $HOME/.bash_aliases.orig; fi
-if [[ -e $HOME/.bash_profile ]]; then mv $HOME/.bash_profile $HOME/.bash_profile.orig; fi
-if [[ -e $HOME/.bash_progs ]]; then mv $HOME/.bash_progs $HOME/.bash_progs.orig; fi
-if [[ -e $HOME/.bashrc ]]; then mv $HOME/.bashrc $HOME/.bashrc.orig; fi
-if [[ -e $HOME/.bash_functions ]]; then mv $HOME/.bash_functions $HOME/.bash_functions.orig; fi
+script_dir=$(pwd)/..
 
-ln -s $HOME/git/lnx_scripts/bash_aliases/.bash_aliases $HOME/.bash_aliases
+if [[ -e $HOME/.bash_aliases ]]; then mv $HOME/.bash_aliases $HOME/.bash_aliases.bak; fi
+if [[ -e $HOME/.bash_profile ]]; then mv $HOME/.bash_profile $HOME/.bash_profile.bak; fi
+if [[ -e $HOME/.bash_progs ]]; then mv $HOME/.bash_progs $HOME/.bash_progs.bak; fi
+if [[ -e $HOME/.bashrc ]]; then mv $HOME/.bashrc $HOME/.bashrc.bak; fi
+if [[ -e $HOME/.bash_functions ]]; then mv $HOME/.bash_functions $HOME/.bash_functions.bak; fi
+if [[ -e $HOME/.config/user-dirs.dirs ]]; then mv $HOME/.config/user-dirs.dirs $HOME/.config/user-dirs.dirs.bak; fi
+if [[ -e $HOME/.config/user-dirs.locale ]]; then mv $HOME/.config/user-dirs.locale $HOME/.config/user-dirs.locale.bak; fi
+
+ln -s $script_dir/bash_aliases/.bash_aliases $HOME/.bash_aliases
 ln -s $HOME/.bashrc $HOME/.bash_profile
-ln -s $HOME/git/lnx_scripts/bash_progs/.bash_progs $HOME/.bash_progs
-ln -s $HOME/git/lnx_scripts/bashrc/.bashrc $HOME/.bashrc
-ln -s $HOME/git/lnx_scripts/bashrc/.bash_functions $HOME/.bash_functions
+ln -s $script_dir/bash_progs/.bash_progs $HOME/.bash_progs
+ln -s $script_dir/bashrc/.bashrc $HOME/.bashrc
+ln -s $script_dir/bashrc/.bash_functions $HOME/.bash_functions
+ln -s $script_dir/user-dirs/user-dirs.dirs $HOME/.config/user-dirs.dirs
+ln -s $script_dir/user-dirs/user-dirs.locale $HOME/.config/user-dirs.locale
