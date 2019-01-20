33cc82da35a75bd296dd82c609a4cbdf618d32ef
tukusejssirs <bolyosovci@gmail.com>
2019-01-20 18:04:34 +0100

    Update: gup.sh

--- bash_functions/gup.sh
+++ bash_functions/gup.sh
@@ -4,37 +4,43 @@
 # version: 1.1
 # date:    20 Jan 2019
 
+# TODO
+# - when no msg is input as arg and ans is no, it won't ask me to input msg
+
 #!/bin/bash
-# Check if there are any changes in local git repo since last commit
-# src: https://stackoverflow.com/a/5143914/3408342
-if [[ ! `git diff-index --quiet HEAD --` ]]; then
-	# Local repo changed
-	if [[ -n $1 ]]; then
-		git add --all
-		git commit -am "$1"
-		git push
+function gup(){
+	# Check if there are any changes in local git repo since last commit
+	# src: https://stackoverflow.com/a/5143914/3408342
+	if [[ ! $(git diff-index --quiet HEAD --) ]]; then
+		# Local repo changed
+		if [[ -n $1 ]]; then
+			git add --all
+			git commit -am "$1"
+			git push
+		else
+			echo "Do you really want to commit the changes without providing a message?"
+			echo "[y]es, [n]o, [a]bort"
+			read ans
+			case ans in
+				y|Y|yes|Yes|YES)
+					git add --all
+					git commit --allow-empty-message -am ""
+					git push
+					;;
+				n|N|no|No|NO)
+					# TODO: this part does not work
+					echo "Provide a commit message please:"
+					read cm
+					git add --all
+					git commit -am "$cm"
+					;;
+				a|A|abort|Abort|ABORT)
+					exit 1
+					;;
+			esac
+		fi
 	else
-		echo "Do you really want to commit the changes without providing a message?"
-		echo "[y]es, [n]o, [a]bort"
-		read ans
-		case ans in
-			y|Y|yes|Yes|YES)
-				git add --all
-				git commit --allow-empty-message -am ""
-				git push
-				;;
-			n|N|no|No|NO)
-				echo "Provide a commit message please:"
-				read cm
-				git add --all
-				git commit -am "$cm"
-				;;
-			a|A|abort|Abort|ABORT)
-				exit 1
-				;;
-		esac
+		# Local repo did not changed
+		echo "There were have been no changes made in the repo."
 	fi
-else
-	# Local repo did not changed
-	echo "There were have been no changes made in the repo."
-fi
+}
