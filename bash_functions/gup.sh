e7a4bdb594cbb198d21a664a945bc8981e2b4d35
tukusejssirs <bolyosovci@gmail.com>
2019-01-20 18:01:42 +0100

    /home/ts/git/lnx_scripts/bash_functions/gup.sh

--- bash_functions/gup.sh
+++ bash_functions/gup.sh
@@ -1,20 +1,40 @@
+# This script checks if there is something to commit; if yes, commit the changes with commit message that is supplied as argument.
+
+# author:  Tukusej's Sirs
+# version: 1.1
+# date:    20 Jan 2019
+
 #!/bin/bash
-function gup(){
-	msg="$@"
-	test=`gs | tail -1`
-	okay="nothing to commit, working tree clean"
-
-	if [[ $test = $okay ]]; then
-		return $okay
-	else
+# Check if there are any changes in local git repo since last commit
+# src: https://stackoverflow.com/a/5143914/3408342
+if [[ ! `git diff-index --quiet HEAD --` ]]; then
+	# Local repo changed
+	if [[ -n $1 ]]; then
 		git add --all
-		git commit -am ${msg}
+		git commit -am "$1"
 		git push
+	else
+		echo "Do you really want to commit the changes without providing a message?"
+		echo "[y]es, [n]o, [a]bort"
+		read ans
+		case ans in
+			y|Y|yes|Yes|YES)
+				git add --all
+				git commit --allow-empty-message -am ""
+				git push
+				;;
+			n|N|no|No|NO)
+				echo "Provide a commit message please:"
+				read cm
+				git add --all
+				git commit -am "$cm"
+				;;
+			a|A|abort|Abort|ABORT)
+				exit 1
+				;;
+		esac
 	fi
-}
-
-function lazygit() {
-    git add --all
-    git commit -am "$1"
-    git push
-}
+else
+	# Local repo did not changed
+	echo "There were have been no changes made in the repo."
+fi
