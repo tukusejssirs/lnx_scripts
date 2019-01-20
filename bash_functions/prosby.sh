6d05ea47c335555007d682f0794a346c7ea77513
tukusejssirs <bolyosovci@gmail.com>
2019-01-20 19:12:09 +0100

    Fix: todoPer was substituted with chckdPer

--- bash_functions/prosby.sh
+++ bash_functions/prosby.sh
@@ -16,7 +16,7 @@ total=$(printf "%3s" $(expr $smv_i + $smv_ii + $chckd))
 smv_iPer=$(LC_ALL=C /usr/bin/printf "%04.1f\n" $(echo "$smv_i * 100 / $total" | bc -l))
 smv_iiPer=$(LC_ALL=C /usr/bin/printf "%04.1f\n" $(echo "$smv_ii * 100 / $total" | bc -l))
 chckdPer=$(LC_ALL=C /usr/bin/printf "%04.1f\n" $(echo "$chckd * 100 / $total" | bc -l))
-todoPer=$(LC_ALL=C /usr/bin/printf "%04.1f\n" $(echo "$chckd * 100 / $total" | bc -l))
+todoPer=$(LC_ALL=C /usr/bin/printf "%04.1f\n" $(echo "$todo * 100 / $total" | bc -l))
 
 echo -e "${lyellow}smv_i${fdefault}         ${lred}smv_ii${fdefault}        ${lgreen}chckd${fdefault}          ${lmagenta}todo${fdefault}           ${lblue}total${fdefault}"
 echo -e "${lyellow}$smv_i ($smv_iPer %)${fdefault}, ${lred}$smv_ii ($smv_iiPer %)${fdefault}, ${lgreen}$chckd ($chckdPer %)${fdefault} + ${lmagenta}$todo ($todoPer %)${fdefault} = ${lblue}$total${fdefault}"
