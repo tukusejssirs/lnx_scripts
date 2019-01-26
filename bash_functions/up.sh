0afc5cc1d400aed33677beaeb475b3b79577f9bb
tukusejssirs <bolyosovci@gmail.com>
2019-01-26 17:26:43 +0100

    Update up.sh to v1.2

--- bash_functions/up.sh
+++ bash_functions/up.sh
@@ -1,14 +1,14 @@
 # This script is used to move up the directories using `up n`, where `n` is an integer, how many dirs should be moved up
 
 # author: Tukusej's Sirs (based upon what I found on the Internet)
-# created: 25 Feb 2018 (v1.0); 1 Jan 2019 (v1.1)
+# created: 25 Feb 2018 (v1.0); 1 Jan 2019 (v1.1); 26 Jan 2019 (v1.2)
 # dependencies: coreutils
-# version: 1.1
+# version: 1.2
 
 function up(){
-	if [[ $1 = "" ]] || [[ $1 -lt 1 ]] ; then echo "Argument must be a positive integer."; fi
-	for n in $(seq 1 $1); do dots+="../"; done
+	if [[ $1 = "" ]]; then d=1; else d=$1; fi
+	if [[ $d -lt 1 ]]; then echo "Argument must be a positive integer."; fi
+
+	for n in $(seq 1 $d); do dots+="../"; done
 	cd $dots
-	n=""
-	dots=""
 }
