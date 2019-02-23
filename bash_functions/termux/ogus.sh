839ae27a6f021978fec069a4fd90d49040c07856
tukusejssirs <ottobolyos@gmail.com>
2019-02-23 09:03:36 +0100

    Revert "Test MD5 addition"
    This reverts commit 421dcfda987fee83e498199649010b2c8e8881ea.

--- bash_functions/termux/ogus.sh
+++ bash_functions/termux/ogus.sh
@@ -6,25 +6,21 @@
 # Btw, this is why I named this script as `ogus`: Open Gapps Update Script
 
 # author:  Tukusej's Sirs
-# version: 1.1
-# date:    22 Feb 2019
+# version: 1.0
+# date:    1 Feb 2019
 
 # dependencies (Android):     termux termux-tasker tasker rooted_android su bash_shell
 # dependencies (Termux/Bash): termux-sudo coreutils grep sed curl jq wget ncurses-utils
 
 # TODO:
 # - make it work without termux
+# - create ogus_init.sh
 # - create an after-boot-up script that would delete the update.zip
-
-# User-dependant variables
-# Note: change according to your needs.
-path="/sdcard/dls"  # Location of downloaded files
 
 # Colour definitions
 fdefault="\e[39m"  # Default format and colour
 lred="\e[91m"      # Light red
 lmagenta="\e[95m"  # Light magenta
-lyellow="\e[93m"   # Light yellow
 
 echo -e "${lmagenta}Checking Open GApps configuration and version ...${fdefault}"
 type=$(sudo cat /system/etc/g.prop | grep ro.addon.type | sed 's/^.*type=\(.*\)$/\1/' -)
@@ -34,40 +30,22 @@ if [[ "$type" == "gapps" ]]; then
 	sdk=$(sudo cat /system/etc/g.prop | grep sdk | sed 's/^.*sdk=\(.*\)$/\1/' -)
 	platform=$(sudo cat /system/etc/g.prop | grep platform | sed 's/^.*platform=\(.*\)$/\1/' -)
 	open_type=$(sudo cat /system/etc/g.prop | grep open_type | sed 's/^.*open_type=\(.*\)$/\1/' -)
- 	curVer=$(sudo cat /system/etc/g.prop | grep version | sed 's/^.*version=\(.*\)$/\1/' -)
+	curVer=$(sudo cat /system/etc/g.prop | grep version | sed 's/^.*version=\(.*\)$/\1/' -)
 	latVer=$(curl --silent "https://api.github.com/repos/opengapps/$arch/releases/latest" | jq -r .tag_name)
 
 	if [[ $curVer < $latVer ]]; then
-		echo -en "${lmagenta}Found newer version ($latVer).\nDownloading ... ${fdefault}"
-		zip="open_gapps-$arch-$platform-$open_type-$latVer.zip"
-		url="https://github.com/opengapps/$arch/releases/download/$latVer/$zip"
-		wget -q $url{,.md5'}
-		echo -e "${lmagenta}Done.${fdefault}"
+		echo -e "${lmagenta}Found newer version ($latVer).\nDownloading ...${fdefault}"
+		wget -q -O /storage/emulated/0/dls/update.zip https://github.com/opengapps/$arch/releases/download/$latVer/open_gapps-$arch-$platform-$open_type-$latVer.zip
 
-		echo -en "${lmagenta}Checking MD5 ... ${fdefault}"
-#		$zip_md5=$(md5sum $zip)
-#		$md5_md5=$(cat $zip.md5)
-#		if [[ $zip_md5 == $md5_md5 ]]; then
-#			echo -e "${lmagenta}done.${fdefault}"
-#		else
-#			echo -e "${lred}\nERROR: MD5 check failed, see bellow.\n\nzip: $zip_md5\nmd5: $md5_md5${fdefault}"
-#			echo
-#			read -t 5 ans
-#			case $ans in
-#				y|Y|yes|Yes|YES) echo -e "${lyellow}WARNING: Ignoring MD5 mismatch.${fdefault}" ;;
-#				*) exit 3
-#			esac
-#		fi
-
-#		echo -e "${lmagenta}Creating Open Recovery Script and rebooting ...${fdefault}"
-#		sudo bash -c "echo -e 'install $path/$zip\nrm $path/$zip\nreboot' > /cache/recovery/openrecoveryscript"
-#		sudo reboot recovery
-#	elif [[ $curVer == $latVer ]]; then
-#		echo -e "${lmagenta}Open GApps version $latVer is already the latest version, no need to update it.${fdefault}"
-#	else
-#		echo -e "${lred}ERROR: Something has happened: current and latest version cannot be compared.\n\ncurrent version = $curVer\nlatest version  = $latVer${fdefault}"
-#		exit 1
-#	fi
+		echo -e "${lmagenta}Download complete.\nCreating Open Recovery Script and rebooting ...${fdefault}"
+		sudo bash -c "echo -e 'install /storage/emulated/0/dls/update.zip\nrm /storage/emulated/0/dls/update.zip\nreboot' > /cache/recovery/openrecoveryscript"
+		#sudo reboot recovery
+	elif [[ $curVer == $latVer ]]; then
+		echo -e "${lmagenta}Open GApps version $latVer is already the latest version, no need to update it.${fdefault}"
+	else
+		echo -e "${lred}ERROR: Something has happened: current and latest version cannot be compared.\n\ncurrent version = $curVer\nlatest version  = $latVer${fdefault}"
+		exit 1
+	fi
 else
 	echo -e "${lred}ERROR: This script only updates Open GApps. On current system, the Open GApps are not installed.${fdefault}"
 	exit 2
