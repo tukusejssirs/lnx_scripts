396810fdfa2ea3f514d597a8901b9cedcee0e44b
tukusejssirs <bolyosovci@gmail.com>
2019-03-21 08:15:04 +0100

    Update gbta.sh

--- bash_functions/gbta.sh
+++ bash_functions/gbta.sh
@@ -1,41 +1,38 @@
-tracked="/tmp/tracked"
-untracked="/tmp/untracked"
+# This function creates a branch that tracks a remote branch of the same name, or only what is needed to have all existing remote branches tracked
 
-mkdir /tmp/{tracked,untracked}
+# author:  Tukusej's Sirs
+# date:    21 March 2019
+# version: 1.0
 
-### orig
-while read branch; do
-#   upstream=$(git rev-parse --abbrev-ref $branch@{upstream} 2>/dev/null)
-#   if [[ $? == 0 ]]; then
-#     echo $branch tracks $upstream
-#   else
-#     echo $branch has no upstream configured
-#   fi
-# done < <(git for-each-ref --format='%(refname:short)' refs/heads/*)
+#function gbta(){
 
-### mdfd
-# while read branch; do
-# 	upstream=$(git rev-parse --abbrev-ref $branch@{upstream} 2>/dev/null)
-# 	if [[ $? == 0 ]]; then
-# 		echo $branch tracks $upstream >> /tmp/tracked
-# 	else
-# 		echo $branch >> /tmp/untracked
-# 	fi
-# done < <(git for-each-ref --format='%(refname:short)' refs/heads/*)
+# TODO
+# - warn about missing HEAD ref
+# - exclude HEAD from remotes
 
 remote_branches=$(git branch -r)
 
+# HEAD test
+if [[ $(echo $remote_branches | grep -o HEAD) == "HEAD" ]]; then
+	# Remove HEAD from the variable
+	remote_branches=$(echo $remote | sed '/HEAD/d' -)
+else
+	# Warn about missing HEAD
+	echo -e "${lyellow}WARNING: No HEAD found.${fdefault}"
+fi
+
 # If remote branch does not exist locally, create it
+# If remote branch exists locally, but is not tracked, make it tracked
 for r in $remote_branches; do
 	branch_name=$(echo $r | grep -o "[^/]*$")
 	branch_test=$(git branch --list $branch_name)
-	if [[ $branch_name != $branch_test ]]; then
-		git checkout --track $r
-	else
-		is_tracking=$(git rev-parse --abbrev-ref $branch_name@{upstream} | echo $?)
+	is_tracking=$(git rev-parse --abbrev-ref $branch_name@{upstream} &>/dev/null | echo $?)
+	if [[ $branch_name != $branch_test || $is_tracking != 0 ]]; then
 		if [[ $is_tracking != 0 ]]; then
-			git checkout --track $r
+			git checkout $branch_name
+		fi
+		git checkout --track $r
+		echo "$branch_name is set to track $r"
 	fi
 done
-
-# rm -rf /tmp/{tracked,untracked}
+#}
