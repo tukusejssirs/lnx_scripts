69c8e21b2cf8f52d6cdef3d6862a70c5a1b05e99
tukusejssirs <bolyosovci@gmail.com>
2019-03-22 15:31:36 +0100

    Add final version

--- bash_functions/gbta.sh
+++ bash_functions/gbta.sh
@@ -9,13 +9,15 @@
 # TODO
 # - warn about missing HEAD ref
 # - exclude HEAD from remotes
+# - make it a function
+# - save current branch a check back to it
 
-remote_branches=$(git branch -r)
+remote_branches=$(git branch -r | sed 's/^[ ]*//g' -)
 
 # HEAD test
-if [[ $(echo $remote_branches | grep -o HEAD) == "HEAD" ]]; then
+if [[ $(echo "$remote_branches" | grep -o HEAD) == "HEAD" ]]; then
 	# Remove HEAD from the variable
-	remote_branches=$(echo $remote | sed '/HEAD/d' -)
+	remote_branches=$(echo "$remote_branches" | sed '/HEAD/d' -)
 else
 	# Warn about missing HEAD
 	echo -e "${lyellow}WARNING: No HEAD found.${fdefault}"
@@ -24,17 +26,22 @@ fi
 # If remote branch does not exist locally, create it
 # If remote branch exists locally, but is not tracked, make it tracked
 for r in $remote_branches; do
+	echo
 	branch_name=$(echo $r | grep -o "[^/]*$")
-	branch_test=$(git branch --list $branch_name)
+	branch_test=$(git branch --list $branch_name | sed 's/^[ *]*//g' -)
 	is_tracking=$(git rev-parse --abbrev-ref $branch_name@{upstream} &>/dev/null | echo $?)
+	echo branch: "$branch_name (test: $branch_test, track: $is_tracking)"
 #	if [[ $branch_name != $branch_test || $is_tracking != 0 ]]; then
 #		if [[ $is_tracking != 0 ]]; then
 		if [[ $branch_name != $branch_test ]]; then
 			git checkout -b $branch_name
+			echo branch created
 		else
-			git checkout $branch_name
+			#git checkout $branch_name
+			echo branch no checkout
 		fi
-		git checkout --track $r
+		#git checkout --track $r
+		git branch -u $r $branch_name
 		echo "$branch_name is set to track $r"
 	#fi
 done
