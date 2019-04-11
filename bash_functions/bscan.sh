3ba829aec615779efc89463db5742bc449cbac96
tukusejssirs <bolyosovci@gmail.com>
2019-04-11 19:32:34 +0200

    Fix bscan bash function

--- bash_functions/bscan.sh
+++ bash_functions/bscan.sh
@@ -9,26 +9,22 @@
 # version: 1.0
 
 function bscan(){
-	Device
-	if [[ $SANE_DEFAULT_DEVICE == "" ]]; then
-		SANE_DEFAULT_DEVICE=$(scanimage -f "%d")
-	fi
-	device=$SANE_DEFAULT_DEVICE
-
 	filename=$(echo "$1" | grep -Po "^.*(?=\.[a-zA-Z]*$)")
 	colour_mode=$2
 	start=$3
 	end=$4
 	digits=$5
 	format=$(echo "$1" | grep -Po "\.\K[a-zA-z]*$")
+	lnx_scripts="$PWD/git/lnx_scripts"
+	source $lnx_scripts/bash_functions/scan.sh
 
 	for n in $(seq $start $end); do
 		num=$(printf "%0*d\n" $digits $n)
 		ans="r"
 		while [ "$ans" = "R" ] || [ "$ans" = "r" ]; do
 			# scanimage [grey|colour|lineart] [filename.ext]
-			scanimage $colour_mode $num_$filename.$format
-			scanimage -d $device -p --resolution 600 --mode Color --format=png --batch-start > $num_$filename.$format
+			scan $colour_mode $num_$filename.$format
+			# scanimage -d $device -p --resolution 600 --mode Color --format=png --batch-start > $num_$filename.$format
 			while read ans && [ "$ans" != "R" ] && [ "$ans" != "r" ] && [ "$ans" != "N" ] && [ "$ans" != "n" ]; do
 				echo -n
 			done
